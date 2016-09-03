4ddbd9ad2f55e26b63b6aa1e1de1b90d95e0f551
Pedro <Pedro@pedros-mbp.home>
2016-09-03 05:49:27 -0300

    changing label text when button is pressed

--- 1st-iOS/ViewController.swift
+++ 1st-iOS/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        myLabel.text = "It worked!"iter
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World!")
     }
 
     override func didReceiveMemoryWarning() {
